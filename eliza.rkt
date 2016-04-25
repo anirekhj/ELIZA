472324fad0914ee4f7fc0189cc5b2840bcc2be5d
Anirekh Jain <anirekhj@outlook.com>
2016-04-25 12:27:26 -0400

    Update eliza.rkt

--- eliza.rkt
+++ eliza.rkt
@@ -1,8 +1,21 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname eliza) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #t)))
-;; checks if list of two symbols are equal
 
+(define (changelist '((i you)
+                      (you me)
+                      (me you)
+                      (was were)
+                      (am are)
+                      (are am)
+                      (yours mine)
+                      (i'd you'd)
+                      (i've you've)
+                      (i'll you'll)
+                      (your my)
+                      (my your))))
+
+;; checks if list of two symbols are equal
 (define (same? l1 l2)
   (local [(define (symbols-equal? l1 l2)
             (cond [(and (empty? l1) (empty? l2)) true]
