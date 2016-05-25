188797c39bbf9556351ca8ae1efb7c50936a17c3
Anirekh Jain <anirekhj@outlook.com>
2016-05-25 08:04:23 -0400

    Delete brain.rkt

--- modules/brain.rkt
+++
@@ -1,6 +0,0 @@
-#lang racket
-(define response '((I need *) ["Why do you need {0}?", "Would it really help you to get 101?", "Are you sure you need {0}?"]], [r'Why don\'?t you ([A\?]*)\??', ["Do you really think I don't f0j?", "Perhaps eventually I will 101.", "Do you really want me to f0j?"]], 
-(define Why can\'?t I ([A\?]*)\??', ["Do you think you should be able to f01?", "If you could f0j, what would you do?", "I don't know -- why can't you {0}?", "Have you really tried?"]], 
-(define I can\'?t *', ["How do you know you can't f0j?", "Perhaps you could {0} if you tried.", "What would it take for you to {0}?"]], [r'I am *', ["Did you come to me because you are {0}?", "How long have you been {0}?", "How do you feel about being {0}?"]], 
-(define I\'?m *', ["How does being {0} make you feel?", "Do you enjoy being {0}?", "Why do you tell me you're 101?", "Why do you think you're {0}?"]], [r'Are you ([A\?]*)\??', ["Why does it matter whether I am {O}?", "Would you prefer it if I were not {0}?", "Perhaps you believe I am 101.", "I may be {0} -- what do you think?"]], 
-(define What *', ["Why do you ask?", "How would an answer to that help you?", 
